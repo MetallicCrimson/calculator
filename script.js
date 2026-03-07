ed949d766227bb3b92606a1093a254b2fa958a77
MetallicCrimson <tamerlein667@gmail.com>
2026-03-07 21:58:58 +0100

    Make basic functions, make HTML mockup

---
+++ script.js
@@ -0,0 +1,37 @@
+let operator, operand1, operand2;
+
+function add(a,b) {
+    return a+b;
+}
+
+function subtract(a,b) {
+    return a-b;
+}
+
+function multiply(a,b) {
+    return a*b;
+}
+
+function divide(a,b) {
+    return a/b;
+}
+
+function operate(operator,a,b) {
+    switch (operator) {
+        case "+":
+            add(a,b);
+            break;
+        case "-":
+            subtract(a,b);
+            break;
+        case "*":
+            multiply(a,b);
+            break;
+        case "/":
+            divide(a,b);
+            break;
+    
+        default:
+            break;
+    }
+}
