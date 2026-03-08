760d909396136db3b14485318efaf4715b500981
MetallicCrimson <tamerlein667@gmail.com>
2026-03-08 01:06:18 +0100

    Refactor some buttons

--- script.js
+++ script.js
@@ -1,42 +1,27 @@
-const button0 = document.querySelector(".button-0");
-const button1 = document.querySelector(".button-1");
-const button2 = document.querySelector(".button-2");
-const button3 = document.querySelector(".button-3");
-const button4 = document.querySelector(".button-4");
-const button5 = document.querySelector(".button-5");
-const button6 = document.querySelector(".button-6");
-const button7 = document.querySelector(".button-7");
-const button8 = document.querySelector(".button-8");
-const button9 = document.querySelector(".button-9");
-const buttonDecimal = document.querySelector(".button-decimal");
-const buttonAdd = document.querySelector(".button-add");
-const buttonSubtract = document.querySelector(".button-subtract");
-const buttonMultiply = document.querySelector(".button-multiply");
-const buttonDivide = document.querySelector(".button-divide");
 const buttonEqual = document.querySelector(".button-equal");
 const buttonClear = document.querySelector(".button-clear");
 const buttonBackspace = document.querySelector(".button-backspace");
 
 const display = document.querySelector("#display");
-
-const digitButtons = [button0, button1, button2, button3, button4, button5, button6, button7, button8, button9];
-
-const operatorButtons = [buttonAdd, buttonSubtract, buttonMultiply, buttonDivide];
+const operatorButtons = Array.from(document.querySelectorAll(".button-operator"));
+const digitButtons = Array.from(document.querySelectorAll(".button-digit"));
 
 let operator, operand1 = 0, operand2 = 0;
 
-// firstDigit or lastDigit
-let phase = "firstDigit";
+// operand1, operator, operand2
+let phase = "operand1";
+
 
 function digitPress(e) {
     let currentDigit = Number(e.target.textContent);
 
-    if (phase === "firstDigit") {
+    if (phase === "operand1") {
         operand1 = operand1*10 + currentDigit;
         console.log(operand1);
 
         display.textContent = operand1;
     } else {
+        phase = "operand2";
         operand2 = operand2*10 + currentDigit;
 
         display.textContent = operand2;
@@ -45,14 +30,23 @@ function digitPress(e) {
 
 function operatorPress(e) {
     console.log(phase);
-    if (phase === "firstDigit") {
-        phase = "lastDigit";
+    if (phase === "operand1") {
+        phase = "operator";
         e.target.style["background-color"] = "#6d126d";
+        operator = e.target.textContent;
+    } else if (phase === "operator") {
+        for (b of operatorButtons) {
+            if (b !== e.target) {
+                b.style["background-color"] = "#000000";
+            } else {
+                b.style["background-color"] = "#6d126d";
+            }
+        }
+
         operator = e.target.textContent;
     } else {
         for (b of operatorButtons) {
             if (b !== e.target) {
-                console.log("Heyo");
                 b.style["background-color"] = "#000000";
             } else {
                 b.style["background-color"] = "#6d126d";
@@ -60,13 +54,21 @@ function operatorPress(e) {
         }
 
         let ans = operate(operator, operand1, operand2);
-        console.log(ans);
-
+        operand1 = ans;
+        operand2 = 0;
+        operator = e.target.textContent;
+        phase = "operator";
 
     }
 }
 
+function equalPress(e) {
 
+}
+
+console.log(typeof(Array.from(document.querySelectorAll(".button-digit"))));
+
+// what
 for (b of digitButtons) {
     b.addEventListener("mousedown", digitPress);
 }
@@ -75,6 +77,8 @@ for (b of operatorButtons) {
     b.addEventListener("mousedown", operatorPress);
 }
 
+
+buttonEqual.addEventListener("mousedown", equalPress);
 
 
 function add(a,b) {
@@ -94,22 +98,27 @@ function divide(a,b) {
 }
 
 function operate(operator,a,b) {
+    let ans;
+
     switch (operator) {
         case "+":
-            return(add(a,b));
+            ans = add(a,b);
             break;
         case "-":
-            return(subtract(a,b));
+            ans = subtract(a,b);
             break;
         case "*":
-            return(multiply(a,b));
+            ans = multiply(a,b);
             break;
         case "/":
-            return(divide(a,b));
+            ans = divide(a,b);
             break;
     
         default:
             break;
     }
+
+    display.textContent = ans;
+    return ans;
 }
 
